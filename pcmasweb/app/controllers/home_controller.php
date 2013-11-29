2957d3d53fc1086ce0e421aecc0b252588e32e58
Aldo Maranon Andrade <aldo.maranon@gbmobile.com>
2013-11-29 10:35:30 -0600

    ...

---
+++ pcmasweb/app/controllers/home_controller.php
@@ -0,0 +1,15 @@
+<?php
+class HomeController extends AppController {
+
+	var $name = 'Home';
+	var $uses = '';    		            
+
+    function beforeFilter() {
+        parent::beforeFilter(); 
+        $this->layout="default";                              
+    }
+
+	function index() {		
+		
+	}		
+}
