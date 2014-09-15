b1e0ba4ccf97b9606b9eee546198ec9bfc376b90
gbmobile <gbmobile@Aldo-MA.local>
2014-09-15 13:42:25 -0500

    ...

--- web.laravel.iscaldocms/app/views/login.blade.php
+++ web.laravel.iscaldocms/app/views/login.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Saastah | Login</title>
+        <title>Aldosworld | Login</title>
         
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>			
