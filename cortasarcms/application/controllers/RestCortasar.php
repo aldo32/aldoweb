cd0441ffaf2839c245dbe3e6fd96f7316f1a3582
Aldo MArañon Andrade <isc.aldo@hotmail.com>
2015-11-01 13:45:25 -0600

    rest
    cambios

--- cortasarcms/application/controllers/RestCortasar.php
+++ cortasarcms/application/controllers/RestCortasar.php
@@ -1,6 +1,22 @@
 <?php
-require(APPPATH'.libraries/REST_Controller.php');
+require(APPPATH.'libraries/REST_Controller.php');
 
 class RestCortasar extends REST_Controller {
- 
+
+    function __construct() {
+		parent::__construct();
+
+		$this->load->model("Model_usuarios", "usuarios");
+	}
+
+    function usuarios_get() {
+        $id = $this->get('id');
+        $aldo = $this->get('aldo');
+
+        $usuarios = $this->usuarios->getUserById(1);
+
+        $response = array("status"=>"ok", "value1"=>"1", "id"=>$id, "aldo"=>$aldo);
+        //$this->response($response);
+        $this->response($usuarios);
+    }
 }
