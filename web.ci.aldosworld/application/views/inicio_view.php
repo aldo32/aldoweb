f0b29bf7832bfc2ba38e27df9c47f7e2bdc5bab1
Aldo Marañon <Aldo Maraon>
2016-10-17 22:26:24 -0500

    aldosworld v2

---
+++ web.ci.aldosworld/application/views/inicio_view.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>title</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+	<style>
+		#background {
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			min-width: 100%;
+			min-height: 100%;
+			width: auto;
+			height: auto;
+			z-index: -100;
+			-webkit-transform: translateX(-50%) translateY(-50%);
+			transform: translateX(-50%) translateY(-50%);
+			background: url(polina.jpg) no-repeat;
+			background-size: cover;
+		}
+		.container h1 {
+			color: white;
+	</style>
+
+</head>
+<body>
+<video autoplay loop muted poster="screenshot.jpg" id="background">
+	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+</video>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="col-md-12">
+
+			<h1>www.viewlike.us</h1>
+
+		</div>
+
+	</div>
+
+</div>
+
+</body>
+</html>
