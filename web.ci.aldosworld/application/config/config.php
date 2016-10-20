c947f40851f69c23f377729c1f73541a6d02683d
Aldo Marañon <amaranon@kichink.com>
2016-10-20 17:33:15 -0500

    ...

--- web.ci.aldosworld/application/config/config.php
+++ web.ci.aldosworld/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://dev.aldosworld.com';
+$config['base_url'] = 'http://dev.test.com';
 
 /*
 |--------------------------------------------------------------------------
--- web.ci.aldosworld/application/views/inicio_view.php
+++ web.ci.aldosworld/application/views/inicio_view.php
@@ -1,40 +1,240 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-	<title>Aldo's world</title>
+    <title>Test</title>
 
-	<script type="text/javascript" src="<?php echo base_url() ?>resources/js/jquery-3.1.1.js"></script>
-	<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url() ?>resources/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url() ?>resources/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>resources/css/hexagons.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>resources/css/main.css">
 
-	<link rel="stylesheet" href="<?php echo base_url() ?>resources/css/bootstrap.css">
-	<link rel="stylesheet" href="<?php echo base_url() ?>resources/css/hexagon.min.css">
-	<link rel="stylesheet" href="<?php echo base_url() ?>resources/css/main.css">
+    <script type="text/javascript" src="<?php echo base_url() ?>resources/js/jquery-3.1.1.js"></script>
+    <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script><!-- Tether for Bootstrap -->
+    <script type="text/javascript" src="<?php echo base_url() ?>resources/js/bootstrap.js"></script>
 
 </head>
 <body>
-<video autoplay loop muted poster="" id="background">
-	<source src="<?php echo base_url(); ?>resources/back-video.mp4" type="video/mp4">
-</video>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../navbar/">Default</a></li>
+                <li><a href="../navbar-static-top/">Static top</a></li>
+                <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
 
-<div class="container">
-	<div class="row">
-		<div class="col-xl-12">
-			<div class="col-xl-3">
-				<div class="hexagon-xl hexagon-default hexagon-hover"></div>
-			</div>
-			<div class="col-xl-3">
-				<div class="hexagon-xl hexagon-default hexagon-hover"></div>
-			</div>
-			<div class="col-xl-3">
-				<div class="hexagon-xl hexagon-default hexagon-hover"></div>
-			</div>
-		</div>
-	</div>
-</div>
+<ul id="hexGrid">
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6139/5986939269_10721b8017.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm4.staticflickr.com/3165/5733278274_2626612c70.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm8.staticflickr.com/7163/6822904141_50277565c3.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm4.staticflickr.com/3771/13199704015_72aa535bd7.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm3.staticflickr.com/2878/10944255073_973d2cd25c.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm3.staticflickr.com/2878/10944255073_973d2cd25c.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+    <li class="hex">
+        <a class="hexIn" href="#">
+            <img src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg" alt="" />
+            <h1>This is a title</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+        </a>
+    </li>
+</ul>
 
 </body>
 </html>
