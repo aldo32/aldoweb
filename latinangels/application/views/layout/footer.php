9e593e607d9df04570c120552bf20c3e89a54f90
Aldo Marañon <aldo@MacBook-Air-de-Aldo.local>
2017-04-05 16:51:07 -0500

    ...

--- latinangels/application/views/layout/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="row wrap-footer">
-    <div class="col-md-12 text-center">
-        <br>
-        <img src="<?php echo base_url("resources/images/logo_footer.png") ?>">
-    </div>
-</div>
